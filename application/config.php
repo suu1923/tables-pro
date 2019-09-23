dc431847968fc9ed91ce50387de05a8394f52c53
Suu1923 <a1538304190@163.com>
2019-09-23 17:29:43 +0800

    更新异常处理

--- application/config.php
+++ application/config.php
@@ -164,7 +164,7 @@ return [
     // 显示错误信息
     'show_error_msg'         => false,
     // 异常处理handle类 留空使用 \think\exception\Handle
-    'exception_handle'       => '',
+    'exception_handle'       => 'app\lib\Exception\ExceptionHandler',
 
     // +----------------------------------------------------------------------
     // | 日志设置
@@ -244,4 +244,12 @@ return [
         'var_page'  => 'page',
         'list_rows' => 15,
     ],
+
+    // 验证码配置
+    'captcha' => [
+        'length' => 4,
+        'fontSize' => 40,
+        'useCurve' => false,
+        'useNoise' => false
+    ]
 ];
